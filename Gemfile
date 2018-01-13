ed9e175576cd9fa350711c521c45023d38064c1a
Nestor Guthrie <nestor33125@hotmail.com>
2018-01-13 04:32:24 -0500

    jquery gem added

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'simple_form'
 gem 'haml'
 gem 'bootstrap-sass'
+gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,7 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-//= require rails-ujs
+//= require jquery3
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
